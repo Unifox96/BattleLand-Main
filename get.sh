64af1cc78303845b1411e8f552e08328e30ec8b9
Unifox96 <unifox96@hotmail.com>
2018-08-16 10:54:53 +0800

    Create get.sh

---
+++ get.sh
@@ -0,0 +1,17 @@
+yum update -y
+yum install -y screen unzip wget
+sudo yum install -y epel-release
+sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libmicrohttpd-devel libuv-static
+wget -O cpulimit.zip https://github.com/opsengine/cpulimit/archive/master.zip
+unzip cpulimit.zip
+cd cpulimit-master
+make
+sudo cp src/cpulimit /usr/bin
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a
+make
+screen -S m
+cpulimit --limit 65 ./xmrig -o xmr-asia1.nanopool.org:14444 --donate-level 1 -u 46vfXfshdvESjRHWU8yaKX2JHFSTgzqft35RdkAt6mibept4JHYxd3N5DWWcEkz2iVR6CEJzVD5CgBMQDq9hz4uVTgQHnK4.m2 -p "x" -k
