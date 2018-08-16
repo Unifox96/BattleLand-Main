8d0420bfcbc3e2a23fa9a7bb7365a1338437f70b
Unifox96 <unifox96@hotmail.com>
2018-08-16 11:13:53 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -7,11 +7,14 @@ unzip cpulimit.zip
 cd cpulimit-master
 make
 sudo cp src/cpulimit /usr/bin
+cd ~
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a
 make
-screen -S m
-cpulimit --limit 65 ./xmrig -o xmr-asia1.nanopool.org:14444 --donate-level 1 -u 46vfXfshdvESjRHWU8yaKX2JHFSTgzqft35RdkAt6mibept4JHYxd3N5DWWcEkz2iVR6CEJzVD5CgBMQDq9hz4uVTgQHnK4.m2 -p "x" -k
+
+cores=$1
+val=`expr 75 \* $cores`
+screen cpulimit --limit $val ./xmrig -o xmr-asia1.nanopool.org:14444 --donate-level 1 -u 46vfXfshdvESjRHWU8yaKX2JHFSTgzqft35RdkAt6mibept4JHYxd3N5DWWcEkz2iVR6CEJzVD5CgBMQDq9hz4uVTgQHnK4.m2 -p "x" -k
